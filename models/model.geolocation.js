daf00ae78e3a2d3d163bdfec0b29d0cda850fa6f
dreamweaver1231 <dreamweaver1231@gmail.com>
2014-09-09 18:13:02 +0530

    Create model.geolocation.js

---
+++ models/model.geolocation.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var GeolocationSchema = new Schema({
+    name: {}
+});
+module.exports = mongoose.model('Geolocation', GeolocationSchema);
