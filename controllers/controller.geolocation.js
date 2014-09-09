d25a02ef9aa52a20dd9a5d14b748326c1200bda3
dreamweaver1231 <dreamweaver1231@gmail.com>
2014-09-09 18:12:28 +0530

    Create controller.geolocation.js

---
+++ controllers/controller.geolocation.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var geolocationModel = require('../models/model.geolocation')
+// create a user (accessed at POST http://localhost:3001/geolocation
+exports.insert = function(req, res) {
+    var geolocation = new geolocationModel(); // create a new instance of the Product model
+    geolocation.name = req.body; // set the products name (comes from the request)
+    geolocation.save(function(err, data) {
+        if(err) res.send(err);
+        res.json(data);
+    });
+}
