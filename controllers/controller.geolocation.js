a6bdd522b3b2df0bdcd45234dcb3ba70889d48ba
dreamweaver1231 <dreamweaver1231@gmail.com>
2014-09-10 09:15:55 +0000

    working geolocation post

--- controllers/controller.geolocation.js
+++ controllers/controller.geolocation.js
@@ -7,7 +7,7 @@ exports.insert = function(req, res) {
     geolocation.name = req.body; // set the products name (comes from the request)
     geolocation.save(function(err, data) {
         if(err) res.send(err);
-        res.json(data);
+        res.json(200);
     });
 
 };
